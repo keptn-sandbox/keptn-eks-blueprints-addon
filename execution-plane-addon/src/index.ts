16893a06d19f736751d0bbb14392c88ee5f18fe3
christian.heckelmann <christian.heckelmann@dynatrace.com>
2022-01-04 13:10:58 +0100

    feature: Parameters for KeptnExecutionPlaneAddOn

--- execution-plane-addon/src/index.ts
+++ execution-plane-addon/src/index.ts
@@ -5,34 +5,41 @@ import { Construct } from '@aws-cdk/core'
  * Configuration options for the add-on.
  */
 
+ type KeptnExecutionPlaneParams = {
+    apiToken: string,
+    namespace: string,
+    helmrepo: string,
+    version: string,
+    controlPlaneUrl: string
+}
+
+const defaultKeptnExecutionPlaneParams: KeptnExecutionPlaneParams = {    
+    apiToken: "",    
+    namespace: "keptn",
+    helmrepo: "https://storage.googleapis.com/keptn-installer",
+    version: "0.11.4",
+    controlPlaneUrl: ""
+}
+
 export class KeptnExecutionPlaneAddOn implements ClusterAddOn {
-    readonly Namespace: string;
-    readonly Version: string;
-    readonly ControlPlaneUrl: string;
-    readonly HelmRepository: string;
-    readonly ApiToken: string;
+    props: KeptnExecutionPlaneParams
 
-    constructor(controlPlaneUrl?: string, apiToken?: string, namespace?: string, helmrepo?: string, version?: string) {
-        this.Namespace = namespace ?? "keptn";
-        this.HelmRepository = helmrepo ?? "https://storage.googleapis.com/keptn-installer"
-
-        this.Version = version ?? "0.11.3"
-        this.ControlPlaneUrl = controlPlaneUrl ?? ""
-        this.ApiToken = apiToken ?? ""
+    constructor(params: Partial<KeptnExecutionPlaneParams>) {
+        this.props = {...defaultKeptnExecutionPlaneParams, ...params}
     }
 
     async deploy(clusterInfo: ClusterInfo): Promise<Construct> {
         return clusterInfo.cluster.addHelmChart("helm-service", {
             chart: "helm-service",
-            repository: this.HelmRepository,
-            version: this.Version,
-            namespace: this.Namespace,
+            repository: this.props.helmrepo,
+            version: this.props.version,
+            namespace: this.props.namespace,
             values: {
                 remoteControlPlane: {
                     enabled: true,
                     api: {
-                        token: this.ApiToken,
-                        hostname: this.ControlPlaneUrl
+                        token: this.props.apiToken,
+                        hostname: this.props.controlPlaneUrl
                     }
                 },
             }
