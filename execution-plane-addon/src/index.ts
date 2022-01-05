1dc3f19ca581b0326a713658f79c992c0d996e32
christian.heckelmann <christian.heckelmann@dynatrace.com>
2022-01-05 08:19:36 +0100

    feature: Adding Secrets Manager Support for Execution Planes

--- execution-plane-addon/src/index.ts
+++ execution-plane-addon/src/index.ts
@@ -1,19 +1,55 @@
 import { ClusterAddOn, ClusterInfo } from '@aws-quickstart/ssp-amazon-eks';
 import { Construct } from '@aws-cdk/core'
+import {getSecretValue, loadExternalYaml} from "@aws-quickstart/ssp-amazon-eks/dist/utils";
+
+
+interface KeptnSecret {
+    API_TOKEN: string;
+    BRIDGE_PASSWORD: string;    
+}
 
 /**
  * Configuration options for the add-on.
  */
 
  type KeptnExecutionPlaneParams = {
+
+    /**
+     * The AWS Secrets Manager Secret which is containing the Keptn bridge password and API Token (keys: API_TOKEN, BRIDGE_PASSWORD)
+     */
+    ssmSecretName: string,
+
+    /**
+     * Keptn API Token is used to connect the Execution Plane to Keptn, not needed if a ssmSecretName is specified
+     */      
     apiToken: string,
+
+    /**
+     * Namespace where the keptn Control Plane will be deployed
+     * @default keptn
+     */    
     namespace: string,
+
+    /**
+     * Helm Repository which will be used for installing Keptn
+     * @default https://storage.googleapis.com/keptn-installer
+     */        
     helmrepo: string,
+
+    /**
+     * The Version of Keptn which should get installed
+     * @default 0.11.4
+     */     
     version: string,
+
+    /**
+     * URL of your Keptn Control Plane
+     */     
     controlPlaneUrl: string
 }
 
 const defaultKeptnExecutionPlaneParams: KeptnExecutionPlaneParams = {    
+    ssmSecretName: "",
     apiToken: "",    
     namespace: "keptn",
     helmrepo: "https://storage.googleapis.com/keptn-installer",
@@ -29,6 +65,13 @@ export class KeptnExecutionPlaneAddOn implements ClusterAddOn {
     }
 
     async deploy(clusterInfo: ClusterInfo): Promise<Construct> {
+
+        if (this.props.ssmSecretName != "") {
+            const secretValue = await getSecretValue(this.props.ssmSecretName, clusterInfo.cluster.stack.region);
+            const credentials: KeptnSecret = JSON.parse(secretValue)
+            this.props.apiToken = credentials.API_TOKEN            
+        }
+
         return clusterInfo.cluster.addHelmChart("helm-service", {
             chart: "helm-service",
             repository: this.props.helmrepo,
